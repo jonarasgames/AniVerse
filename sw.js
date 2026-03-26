2340acb99e1c8a899531e6179967fe45f69d1526
Jonaras Games <79364665+jonarasgames@users.noreply.github.com>
2026-03-25 21:08:35 -0300

    Add Credits action to cancel auto-next during ending countdown

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'aniverse-static-v13';
-const RUNTIME_CACHE = 'aniverse-runtime-v13';
+const STATIC_CACHE = 'aniverse-static-v14';
+const RUNTIME_CACHE = 'aniverse-runtime-v14';
 const MEDIA_CACHE = 'aniverse-media-v1';
 const STREAM_PROXY_PATH = '/__anv_stream_proxy__';
 
